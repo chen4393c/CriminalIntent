191d81bbea46f122608260da7c11342b4ee80003
chen4393c <chen4393c@gmail.com>
2018-07-30 13:30:32 -0500

    Putting an extra

--- app/src/main/java/com/bignerdranch/android/criminalintent/controller/list/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/controller/list/CrimeListFragment.java
@@ -69,7 +69,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
             getActivity().startActivity(intent);
         }
 
