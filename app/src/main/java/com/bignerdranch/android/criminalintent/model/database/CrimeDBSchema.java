9551e70d0d06a0395a02f95cfb8fb2aeb3c8e72f
chen4393c <chen4393c@gmail.com>
2018-08-05 12:32:42 -0500

    Building Your Initial Database

--- app/src/main/java/com/bignerdranch/android/criminalintent/model/database/CrimeDBSchema.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/model/database/CrimeDBSchema.java
@@ -1,13 +1,14 @@
 package com.bignerdranch.android.criminalintent.model.database;
 
 public class CrimeDBSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
 
-    public static final String NAME = "crimes";
-
-    public static final class Cols {
-        public static final String UUID = "uuid";
-        public static final String TITLE = "title";
-        public static final String DATE = "date";
-        public static final String SOLVED = "solved";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
     }
 }
