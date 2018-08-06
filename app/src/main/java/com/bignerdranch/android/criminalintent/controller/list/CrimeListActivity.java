3b4985b3fd7febeeec0b9823bdaa62b3df483f0e
chen4393c <chen4393c@gmail.com>
2018-08-06 15:44:01 -0500

    Creating a layout with two fragment containers

--- app/src/main/java/com/bignerdranch/android/criminalintent/controller/list/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/controller/list/CrimeListActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent.controller.list;
 
 import android.support.v4.app.Fragment;
 
+import com.bignerdranch.android.criminalintent.R;
 import com.bignerdranch.android.criminalintent.controller.SingleFragmentActivity;
 
 public class CrimeListActivity extends SingleFragmentActivity {
@@ -9,5 +10,10 @@ public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_twopane;
     }
 }
