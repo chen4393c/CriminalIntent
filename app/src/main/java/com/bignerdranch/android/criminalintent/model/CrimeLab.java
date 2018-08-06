d49bf46e7c21ce0ec24492678ea77f66821c4856
chen4393c <chen4393c@gmail.com>
2018-08-06 12:14:31 -0500

    Designating a picture location

--- app/src/main/java/com/bignerdranch/android/criminalintent/model/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/model/CrimeLab.java
@@ -9,6 +9,7 @@ import com.bignerdranch.android.criminalintent.model.database.CrimeBaseHelper;
 import com.bignerdranch.android.criminalintent.model.database.CrimeCursorWrapper;
 import com.bignerdranch.android.criminalintent.model.database.CrimeDBSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -75,6 +76,11 @@ public class CrimeLab {
         } finally {
             cursor.close();
         }
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
     }
 
     public void updateCrime(Crime crime) {
